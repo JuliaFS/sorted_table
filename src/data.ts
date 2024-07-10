bc6ce25d2904241a0f65744bd8e49cba0863777d
Yulia <yuliya.f.s@gmail.com>
2024-07-10 16:12:49 +0300

    add property to test sorting with numbers

--- src/data.ts
+++ src/data.ts
@@ -4,69 +4,79 @@ export const data = [
       "name": "Lloyd Matthews",
       "company": "ZIALACTIC",
       "active": true,
-      "country": "Haiti"
+      "country": "Haiti",
+      "price": 1000
     },
     {
       "id": 1,
       "name": "Hampton Gilmore",
       "company": "FURNITECH",
       "active": false,
-      "country": "Iran"
+      "country": "Iran",
+      "price": 2300
     },
     {
       "id": 2,
       "name": "Maude Rhodes",
       "company": "OULU",
       "active": false,
-      "country": "Benin"
+      "country": "Benin",
+      "price": 1080
     },
     {
       "id": 3,
       "name": "Best Hernandez",
       "company": "CRUSTATIA",
       "active": false,
-      "country": "Cote D'Ivoire (Ivory Coast)"
+      "country": "Cote D'Ivoire (Ivory Coast)",
+      "price": 3400
     },
     {
       "id": 4,
       "name": "Pace Gaines",
       "company": "WAZZU",
       "active": true,
-      "country": "Honduras"
+      "country": "Honduras",
+      "price": 1035
     },
     {
       "id": 5,
       "name": "Edna Whitney",
       "company": "QUAREX",
       "active": false,
-      "country": "Tajikistan"
+      "country": "Tajikistan",
+      "price": 800
     },
     {
       "id": 6,
       "name": "Hallie Rowe",
       "company": "DIGINETIC",
       "active": true,
-      "country": "Togo"
+      "country": "Togo",
+      "price": 200
     },
     {
       "id": 7,
       "name": "Audrey Francis",
       "company": "MUSANPOLY",
       "active": false,
-      "country": "Germany"
+      "country": "Germany",
+      "price": 5
     },
     {
       "id": 8,
       "name": "Wanda Hines",
       "company": "OATFARM",
       "active": true,
-      "country": "Nigeria"
+      "country": "Nigeria",
+      "price": 1036
     },
     {
       "id": 9,
       "name": "Castaneda Wolf",
       "company": "ENERSAVE",
       "active": true,
-      "country": "Iraq"
+      "country": "Iraq",
+      "price": 0.06
     }
   ];
